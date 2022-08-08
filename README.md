401e32b81aafa7c9f118278141e769b6597d2493
Rafael Freitas <rafaelcerqueira92@gmail.com>
2022-08-08 09:23:23 -0300

    Utilizando Strategy/praticando com Java "puro".

--- README.md
+++ README.md
@@ -8,3 +8,5 @@
 
 #### Utilizando Singleton
 - Permite a criação de uma úncia instância de uma classe e fornece um modo para recuperá-la.
+#### Utilizando Strategy
+- Simplifica a variação de algoritmos para a resolução de um mesmo problema.
--- src/one/digitalinnovation/gof/Test.java
+++ src/one/digitalinnovation/gof/Test.java
@@ -1,11 +1,26 @@
 package one.digitalinnovation.gof;
 
+import one.digitalinnovation.gof.singleton.SingletonLazy;
+import one.digitalinnovation.gof.strategy.*;
+
 public class Test {
 
     public static void main(String[] args) {
+
+        //singleton
         SingletonLazy lazy = SingletonLazy.getInstancia();
         System.out.println(lazy);
         lazy = SingletonLazy.getInstancia();
         System.out.println(lazy);
+
+        //strategy
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
     }
 }
